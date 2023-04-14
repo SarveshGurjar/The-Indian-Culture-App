e3c91b82d7e17076ae15f11fbb7ebece1714dd31
sarvesh <sarevsh.gurjar15694@sakec.ac.in>
2023-04-14 11:36:08 +0530

    my version

--- app/src/main/java/com/example/agencyapp/DashboardActivity.java
+++ app/src/main/java/com/example/agencyapp/DashboardActivity.java
@@ -2,13 +2,20 @@ package com.example.agencyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class DashboardActivity extends AppCompatActivity {
-
+LinearLayout btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        btn = findViewById(R.id.artbtn);
+
+        btn.setOnClickListener(v->{
+            startActivity(new Intent(this,artactivity.class));
+        });
     }
 }
